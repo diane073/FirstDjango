54762cf728b27841e1ce3d669abfbf0505fd76b6
diane073 <bear9473@gmail.com>
2023-04-07 14:31:51 +0900

    admin에 list display 추가

--- product/admin.py
+++ product/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
 from .models import ProductModel
 
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price',)
+
+
 # Register your models here.
 admin.site.register(ProductModel)
